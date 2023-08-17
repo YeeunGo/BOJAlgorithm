48c80dbf642d756806923203322265a2715d0b9c
YeeunGo <124798613+YeeunGo@users.noreply.github.com>
2023-08-17 16:42:51 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int A = Integer.parseInt(br.readLine());
+		int sum=0;
+		for(int i=1; i <= A; i++) {
+			sum+=i;
+			
+		}
+		bw.write(String.valueOf(sum));
+		bw.flush();
+		
+		
+	}
+
+}
