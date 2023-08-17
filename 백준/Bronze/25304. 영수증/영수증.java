b01b90e453a1d72294b7dcdb520712c267f3ea2b
YeeunGo <124798613+YeeunGo@users.noreply.github.com>
2023-08-17 16:48:51 +0900

    [Bronze IV] Title: 영수증, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for(int i = 1 ; i <= B; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sum += a*b;
+		}
+		
+		if(A == sum) {
+			bw.write("Yes");
+		} else {
+			bw.write("No");
+		}
+		
+		bw.flush();
+		
+	}
+}
