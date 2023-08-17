404102ff1dbb1928064deca8a146767da646c129
YeeunGo <124798613+YeeunGo@users.noreply.github.com>
2023-08-17 17:04:53 +0900

    [Bronze V] Title: A+B - 8, Time: 180 ms, Memory: 16536 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int A = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for(int i = 1 ; i <= A ; i ++) {
+			String strnum = br.readLine();
+			String[] strnumArray = strnum.split(" ");
+			sum = Integer.parseInt(strnumArray[0])+Integer.parseInt(strnumArray[1]);
+			bw.write("Case #"+i+": "+strnumArray[0]+" + "+strnumArray[1]+" = "+sum);
+			bw.newLine();
+		}
+		bw.flush();
+		
+	}
+
+}
